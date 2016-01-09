69c2b07b335245f05c10f52b97ce3b0b0e8ed6ec
tsubaki <ruitomo.ex@gmail.com>
2016-01-10 04:08:34 +0900

    first commit

---
+++ Assets/SpawnTest/FollowTargets.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowTargets : MonoBehaviour {
+
+	[SerializeField]
+	NavMeshAgent agent;
+
+	GameObject player;
+
+	void Start()
+	{
+		player = GameObject.FindWithTag ("Player");
+		Destroy (gameObject, 5);
+	}
+
+	void Reset()
+	{
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update()
+	{
+		agent.SetDestination (player.transform.position);
+	}
+}
---
+++ Assets/SpawnTest/SpawnTest.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class SpawnTest : MonoBehaviour 
+{
+	private IEnumerator spawnFlow = null;
+	private CullingGroup cullingGroup = null;
+
+	[SerializeField] Transform[] targetPositions = null;
+	[SerializeField] GameObject spawnObject = null;
+	[SerializeField] float interval = 1;
+
+	void Awake()
+	{
+		spawnFlow = SpawnCoroutine();
+	}
+
+	void OnEnable()
+	{
+		// cullinggroupを生成してコルーチンを開始
+		SetupCullinggroup ();
+		StartCoroutine (spawnFlow);
+	}
+
+	void OnDisable()
+	{
+		// コルーチンを停止してcullinggroupを破棄
+		StopCoroutine (spawnFlow);
+		cullingGroup.Dispose ();
+		cullingGroup = null;
+	}
+
+	void SetupCullinggroup()
+	{
+		// culling groupの初期化
+		cullingGroup = new CullingGroup ();
+		cullingGroup.targetCamera = Camera.main;
+
+		// 敵を生成する座標の登録
+		BoundingSphere[] bounds = new BoundingSphere[targetPositions.Length];
+		for (int i = 0; i < targetPositions.Length; i++) {
+			bounds [i].position = targetPositions [i].position;
+			bounds [i].radius = 1;
+		}
+		cullingGroup.SetBoundingSpheres (bounds);
+		cullingGroup.SetBoundingSphereCount (targetPositions.Length);
+	}
+
+	IEnumerator SpawnCoroutine()
+	{
+		List<int> countList = new List<int> ();
+		while (true) {
+
+			// 視界外にある生成ポイント一覧を取得
+			countList.Clear ();
+			for (int i = 0; i < targetPositions.Length; i++) {
+				if (cullingGroup.IsVisible (i) == false ) {
+					countList.Add (i);
+				}
+			}
+			if (countList.Count != 0) {
+				var newPos = countList[Random.Range (0, countList.Count)];
+				GameObject.Instantiate (spawnObject, targetPositions [newPos].position, Quaternion.identity);
+			}
+
+			// 0.5秒待つ
+			yield return new WaitForSeconds (interval);
+		}
+	}
+}
